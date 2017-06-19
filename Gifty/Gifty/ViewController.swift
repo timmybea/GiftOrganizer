dddbe3226443ab940486613ede169c8b6f904690
Tim Beals <tim.beals@gmail.com>
2017-06-19 16:36:41 -0700

    first commit: new proj

---
+++ Gifty/Gifty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gifty
+//
+//  Created by Tim Beals on 2017-06-19.
+//  Copyright © 2017 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
