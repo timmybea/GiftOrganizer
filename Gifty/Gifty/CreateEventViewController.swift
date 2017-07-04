52f1280005b11a094c0618c9ffee486509ba3257
Tim Beals <tim.beals@gmail.com>
2017-07-04 10:51:33 -0700

    create control methods for evetnt actions

--- Gifty/Gifty/CreateEventViewController.swift
+++ Gifty/Gifty/CreateEventViewController.swift
@@ -46,12 +46,12 @@ class CreateEventViewController: CustomViewController {
         let actions = UILabel(frame: CGRect(x: pad, y: currMaxY, width: view.bounds.width - pad - pad, height: 40))
         actions.textColor = UIColor.white
         actions.font = FontManager.subtitleText
-        actions.text = "Actions"
+        actions.text = "Actions:"
         view.addSubview(actions)
         
         currMaxY += actions.frame.height + pad
         let oneFourthViewWidth = view.bounds.width / 4
-        let size: CGFloat = 60
+        let size: CGFloat = 45
 
         addGiftImageControl = CustomImageControl(frame: CGRect(x: oneFourthViewWidth - (size / 2), y: currMaxY, width: size, height: size))
         addGiftImageControl.imageView.image = UIImage(named: ImageNames.addGift.rawValue)?.withRenderingMode(.alwaysTemplate)
@@ -71,10 +71,14 @@ class CreateEventViewController: CustomViewController {
         addPhoneImageControl.tintColor = ColorManager.lightText
         view.addSubview(addPhoneImageControl)
         
+        addGiftImageControl.addTarget(self, action: #selector(addGiftTouched), for: .touchUpInside)
+        addCardImageControl.addTarget(self, action: #selector(addCardTouched), for: .touchUpInside)
+        addPhoneImageControl.addTarget(self, action: #selector(addPhoneTouched), for: .touchUpInside)
+        
     }
     
     func didTapBackground() {
-        
+        dropDown.finishEditingTextField()
     }
 }
 
@@ -93,4 +97,25 @@ extension CreateEventViewController: DropDownTextFieldDelegate {
         self.celebrationType = option
         print(celebrationType)
     }
+}
+
+extension CreateEventViewController {
+    
+    func addGiftTouched() {
+        addGiftImageControl.imageView.tintColor = addGiftImageControl.isImageSelected ? ColorManager.lightText : UIColor.white
+        addGiftImageControl.isImageSelected = !addGiftImageControl.isImageSelected
+    }
+    
+    func addCardTouched() {
+        addCardImageControl.imageView.tintColor = addCardImageControl.isImageSelected ? ColorManager.lightText : UIColor.white
+        addCardImageControl.isImageSelected = !addCardImageControl.isImageSelected
+    
+    }
+    
+    func addPhoneTouched() {
+        addPhoneImageControl.imageView.tintColor = addPhoneImageControl.isImageSelected ? ColorManager.lightText : UIColor.white
+        addPhoneImageControl.isImageSelected = !addPhoneImageControl.isImageSelected
+    }
+    
+    
 }
