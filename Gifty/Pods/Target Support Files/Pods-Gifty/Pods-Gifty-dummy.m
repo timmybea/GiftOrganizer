b409f17f976db703f650019a9139be8fb42345b2
Tim Beals <tim.beals@gmail.com>
2017-09-11 19:49:31 -0700

    removing pod files from repo

--- Gifty/Pods/Target Support Files/Pods-Gifty/Pods-Gifty-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Gifty : NSObject
-@end
-@implementation PodsDummy_Pods_Gifty
-@end
--- JTCalendarProgrammatic/Pods/Target Support Files/Pods-JTCalendarProgrammatic/Pods-JTCalendarProgrammatic-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JTCalendarProgrammatic : NSObject
-@end
-@implementation PodsDummy_Pods_JTCalendarProgrammatic
-@end
