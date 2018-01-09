e0abd6cda69a23bfaeb1575e5934e51ddaf837c9
timmybea <tim.beals@gmail.com>
2018-01-09 12:27:06 -0800

    basic setup of today extension with tableview and dummy data

---
+++ Gifty/GiftyTodayExtension/DemoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DemoTableViewCell.swift
+//  GiftyTodayExtension
+//
+//  Created by Tim Beals on 2018-01-09.
+//  Copyright © 2018 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class DemoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Gifty/GiftyTodayExtension/TodayViewController.swift
@@ -0,0 +1,82 @@
+//
+//  TodayViewController.swift
+//  GiftyTodayExtension
+//
+//  Created by Tim Beals on 2018-01-09.
+//  Copyright © 2018 Tim Beals. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    var datasource = ["first item", "second item", "third item"]
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.backgroundColor = UIColor.clear
+        tv.separatorStyle = .none
+        return tv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+        
+        layoutSubviews()
+    }
+    
+    private func layoutSubviews() {
+        
+        view.addSubview(tableView)
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+
+        tableView.register(DemoTableViewCell.self, forCellReuseIdentifier: "demoCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+    
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        if activeDisplayMode == .expanded {
+            preferredContentSize = CGSize(width: 0.0, height: 300.0)
+        } else {
+            preferredContentSize = maxSize
+        }
+    }
+}
+
+extension TodayViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datasource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "demoCell") as! DemoTableViewCell
+        cell.backgroundColor = UIColor.blue
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.text = datasource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30
+    }
+}
